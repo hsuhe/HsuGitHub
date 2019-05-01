4cc725f58898bf3913a3ca7067ddc8281cda9cd2
和 徐 <40127121@gm.nfu.edu.tw>
2019-05-01 10:24:16 +0800

    加入專案檔案。

---
+++ 繼承/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 繼承
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            B x = new B();
+        }
+    }
+    class A
+    {
+        public A()
+        {
+            MessageBox.Show("建構子A開始");
+        }
+        ~A()
+        {
+            MessageBox.Show("解構子A開始");
+        }
+    }
+    class B:A
+    {
+        public B()
+        {
+            MessageBox.Show("建構子B開始");
+        }
+        ~B()
+        {
+            MessageBox.Show("解構子B開始");
+        }
+    }
+
+}
